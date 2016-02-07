f9fd3f9bf2b485b174d013cb646f68bc06d111f6
David Best <david.best@gsa.gov>
2016-02-06 21:25:27 -0500

    Prettifying the code.

--- dayton/spiders/reap_spider.py
+++ dayton/spiders/reap_spider.py
@@ -8,13 +8,14 @@ import os
 import scrapy
 from scrapy.selector import Selector
 from dayton.items import ReapItem
-#from scrapy.shell import inspect_response
+from scrapy.shell import inspect_response
 
 YEAR = 2016
 BASE_XPATH = "//*[@class='main-wrap clearfix']/div[@class='entry col-md-9']/article[@class='post clearfix']"
 TAX_ELIGIBLE_XPATH = BASE_XPATH + "/table[2]/tr/td/table[3]/tr[2]/td[1]/text()"
 TAX_ELIGIBLE_XPATH2 = BASE_XPATH + "/table[2]/tr/td/table[3]/tr[2]/td[2]/text()"
 PAYMENT_PLAN_XPATH = BASE_XPATH + "/div/form/div[1]/tr/td/font/font/b/text()"
+AMOUNT_DUE_XPATH = BASE_XPATH + "/div/form/table[4]/tr[last()]/td[last()]/b/text()"
 
 def year_in_range(year):
     return year == YEAR - 2 or year == YEAR - 3
@@ -24,6 +25,31 @@ def single(array):
 
 def zero(array):
     return "$0.00" in array[0]
+
+def single_and_zero(array):
+    return single(array) and zero(array)
+
+def real(array):
+    return "Real" in array[0]
+
+def single_and_real(array):
+    return single(array) and real(array)
+
+def get_val_year(row):
+    return row.xpath('td[1]/text()').extract()
+
+def get_val_type(row):
+    return row.xpath('td[2]/b/text()').extract()
+
+def get_val_paid(row):
+    return row.xpath('td[5]/text()').extract()
+
+def get_val_due(row):
+    return row.xpath('td[6]/text()').extract()
+
+def is_zero_due(sel):
+    return single_and_zero(
+        sel.xpath(AMOUNT_DUE_XPATH).extract())
 
 class ReapSpider(scrapy.Spider):
     name = 'reap'
@@ -80,38 +106,41 @@ class ReapSpider(scrapy.Spider):
         item = response.meta['item']
         item['payment_plan'] = False
         item['payment_window'] = False
+        item['lastYear'] = YEAR
+
         for paymentplan in sel.xpath(PAYMENT_PLAN_XPATH).extract():
             item['payment_plan'] = 'Unapplied Payments:' in paymentplan or item['payment_plan']
         first_year = None
-        item['lastYear'] = YEAR
         rows = sel.xpath('//form/table[4]')
-        for row in rows.xpath('tr'):
-            val = row.xpath('td[1]/text()').extract()
-            if single(val):
-                year = int(val[0])
-            else:
-                continue
-            if first_year is None:
-                item['lastYear'] = year - 1
-                first_year = year - 1
-            # Data points
-            # td[2]/text() is 'Real/Project' - 'real' represents tax data that we care about
-            # td[5]/text() is 'Payments' - how we tell if a payment was made
-            val_type = row.xpath('td[2]/b/text()').extract()
-            val = row.xpath('td[5]/text()').extract()
-            val_due = row.xpath('td[6]/text()').extract()
-            if single(val_type) and val_type[0] == u'Real\xa0':
-                # Rules:
-                # Last two full tax years have no payments
-                if year_in_range(year) and (
-                        (single(val) and not zero(val)) or (single(val_due) and zero(val_due))):
-                    item['payment_window'] = True
-                if single(val) and not zero(val) and single(val_due) and zero(val_due):
-                    item['lastYear'] = year
-                elif single(val) and not zero(val):
+        #inspect_response(response, self)
+        if is_zero_due(sel):
+            item['payment_window'] = True
+        else:
+            for row in rows.xpath('tr'):
+                val_year = get_val_year(row)
+                if single(val_year):
+                    year = int(val_year[0])
+                else:
+                    continue
+                if first_year is None:
                     item['lastYear'] = year - 1
-            else:
-                continue
+                    first_year = year - 1
+
+                val_type = get_val_type(row)
+                if single_and_real(val_type):
+                    val_paid = get_val_paid(row)
+                    val_due = get_val_due(row)
+                    # Rules:
+                    # Last two full tax years have no payments
+                    if year_in_range(year) and (
+                            (single(val_paid) and not zero(val_paid)) or (single_and_zero(val_due))):
+                        item['payment_window'] = True
+                    if single(val_paid) and not zero(val_paid) and single_and_zero(val_due):
+                        item['lastYear'] = year
+                    elif single(val_paid) and not zero(val_paid):
+                        item['lastYear'] = year - 1
+                else:
+                    continue
 
         reapitems = csv.writer(open('reapitems.csv', 'ab'),
                                delimiter=',', quoting=csv.QUOTE_MINIMAL)
