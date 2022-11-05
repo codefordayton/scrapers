924edb75a2680f503df7f7d02d34c0f5b4c820d6
Dave Best <dave.e.best@gmail.com>
2022-11-05 19:14:27 -0400

    updating the process for montylots.

--- scrape.sh
+++ scrape.sh
@@ -9,6 +9,19 @@ rm reapitems.csv
 
 echo Starting the scraper.
 scrapy runspider dayton/spiders/reap_spider.py -s DOWNLOAD_DELAY=0.05
-python split_reap_items.py
+# python split_reap_items.py
+
+echo Creating the JSON file.
+cp reapitems.csv ../daytonreap/
+cd ../daytonreap
+python scripts/buildjson.py
+
+echo Building the montylots site.
+cp georeaps.json ../montylots/_resource/reaps.json
+cd ../montylots
+npm run build
+git add .
+git commit -m "Updating the data files."
+git push origin main
 
 deactivate
